b83f7ef269799dc1f3d6ae498bfadcdfd0a077ce
rbarak <rbarak@jsonar.com>
2018-03-29 14:58:35 -0700

    refactoring

--- pygsheets/aws_usage.py
+++ pygsheets/aws_usage.py
@@ -109,11 +109,47 @@ def s3_worksheet_creation(spread_sheet):
             cells_data.append([cur['Name'], creation_date, region_h])
     populate_cells(start_cell='A2', cells_data=cells_data, worksheet=worksheet)
 
-    
+
+def worksheet_creation(spread_sheet, header_data, worksheet_name, worksheet_rows=10, worksheet_cols=8, REGIONS=REGIONS, REGIONS_H=REGIONS_H):
+    worksheet = spread_sheet.add_worksheet(worksheet_name, rows=worksheet_rows, cols=worksheet_cols, src_tuple=None, src_worksheet=None, index=None)
+    populate_headers(headers_data=header_data, worksheet=worksheet)
+    return worksheet
+
+
+"""
+def get_data(debug_header, reg=REGIONS, reg_h=REGIONS_H):   
+    cells_data = list()
+    for r in range(len(reg)):
+        region = reg[r]
+        region_h = reg_h[r]
+        print()
+        print(debug_header)
+        #print("Images in {}".format(region_h))
+        #print("-----------------------")
+        print("-" * len(debug_header)
+        ec2 = boto3.resource('ec2', region_name=region)
+        images = ec2.images.filter(Owners=['self'])
+        for image in images:
+            print("[{}] ( {} {} {} {} )".format(image.state, image.id, image.image_type, image.architecture, image.description, image.platform))
+            cells_data.append([image.state, image.id, image.image_type, image.architecture, image.description, image.platform, region_h])
+    populate_cells(start_cell='A2', cells_data=cells_data, worksheet=worksheet)
+"""
+
+
+def print_debug_headers(r, debug_header, region_h, reg=REGIONS):
+    region = reg[r]
+    #region_h = reg_h[r]
+    print()
+    print(debug_header)
+    #print("Images in {}".format(region_h))
+    #print("-----------------------")
+    print("-" * len(debug_header))
+    return region
+ 
 def images_worksheet_creation(spread_sheet):
-    worksheet = spread_sheet.add_worksheet("AMI", rows=10, cols=8, src_tuple=None, src_worksheet=None, index=None)
     Header = collections.namedtuple('Header', 'cell name bold')
     header_data = dict()
+    cells_data = list()
     header_data[1] = Header(cell='A1', name='Image state', bold=True)
     header_data[2] = Header(cell='B1', name='Image ID', bold=True)
     header_data[3] = Header(cell='C1', name='Image type', bold=True)
@@ -122,43 +158,36 @@ def images_worksheet_creation(spread_sheet):
     header_data[6] = Header(cell='F1', name='Image platform', bold=True)
     header_data[7] = Header(cell='G1', name='Region', bold=True)
     header_data[8] = Header(cell='H1', name='WorksheetCreated: %s' % currentDT, bold=False)
-    populate_headers(headers_data=header_data, worksheet=worksheet)
+    worksheet = worksheet_creation(spread_sheet, header_data, worksheet_name="AMI", worksheet_rows=10, worksheet_cols=8)
 
-    cells_data = list()
     for r in range(len(REGIONS)):
+        region_h = REGIONS_H[r]
+        debug_header = "Images in {}".format(region_h)
+        #print_debug_headers(debug_header="Images in {}".format(region_h), reg_h=REGIONS_H, reg=REGIONS)
+        region = print_debug_headers(r, debug_header=debug_header, reg=REGIONS, region_h=REGIONS_H)
+        """
         region = REGIONS[r]
         region_h = REGIONS_H[r]
         print()
         print("Images in {}".format(region_h))
         print("-----------------------")
-        #rds = boto3.setup_default_session(region_name=region)
-        #rds = boto3.client('rds')
+        """
         ec2 = boto3.resource('ec2', region_name=region)
         images = ec2.images.filter(Owners=['self'])
         for image in images:
             print("[{}] ( {} {} {} {} )".format(image.state, image.id, image.image_type, image.architecture, image.description, image.platform))
-            """
-        s3 = boto3.client('s3', region_name=region)
-        buckets = s3.list_buckets()
-        for i in range(len(buckets['Buckets'])):
-            cur = buckets['Buckets'][i]
-            print(cur['Name'],"("+str(cur['CreationDate'])+")")
-            creation_date = json.dumps(cur['CreationDate'], indent=4, sort_keys=True, default=str)
-            """
-
-            #cells_data.append([cur['Name'], creation_date, region_h])
             cells_data.append([image.state, image.id, image.image_type, image.architecture, image.description, image.platform, region_h])
     populate_cells(start_cell='A2', cells_data=cells_data, worksheet=worksheet)
 
 
 if __name__ == "__main__":
     spread_sheet = create_spreadsheet(spreadsheet_name = "jSonar AWS usage", outh_file = '../client_secret.json')
-    #spread_sheet.link(syncToCloud=False)
 
     images_worksheet_creation(spread_sheet)
     s3_worksheet_creation(spread_sheet)
     security_groups_worksheet_creation(spread_sheet)
-    spread_sheet.del_worksheet(spread_sheet.sheet1)
+    # Delete the default sheet1
+    spread_sheet.del_worksheet(spread_sheet.sheet1) 
 
     #spread_sheet.del_worksheet(spread_sheet.sheet1)
     # share the sheet by email
